4ad1049519eca76404c80c0320d9f946bdf2dd4b
YH <YH@YHdeMac-mini.local>
2016-11-07 18:48:26 +0800

    tools

--- IOS/autoIPAForsvn.py
+++ IOS/autoIPAForsvn.py
@@ -2,7 +2,7 @@ import sys;
 import os;
 
 PROJECT_NAME="KOL";
-SVN_PATH="..\..";
+SVN_PATH="../..";
 Script = "buildIPAForsvn.py";
-UNITY_PROJECT_PATH="..\..";
-os.system("python ..\subversion\subversion.py " + SVN_PATH + " " + Script + " " + UNITY_PROJECT_PATH + " " + PROJECT_NAME);
+UNITY_PROJECT_PATH="../..";
+os.system("python ../subversion/subversion.py " + SVN_PATH + " " + Script + " " + UNITY_PROJECT_PATH + " " + PROJECT_NAME);
