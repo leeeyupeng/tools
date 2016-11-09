eb836af86a6c997c1fa95b337a78ca7345d148d2
leeyupeng <leeyupeng@126.com>
2016-11-09 12:14:06 +0800

    tools

--- Arts/Arts2Res.py
+++ Arts/Arts2Res.py
@@ -15,7 +15,7 @@ os.chdir(UNITY_PROJECT_PATH);
 
 # os.system("ls")
 print UNITY+" -quit -batchmode -projectPath " + os.getcwd() + " -executeMethod ActorEditor.AutoGen Start";
-os.system(UNITY+" -quit -batchmode -executeMethod ActorEditor.AutoGen");
+os.system(UNITY+" -quit -batchmode -projectPath " + os.getcwd() + " -executeMethod ActorEditor.AutoGen");
 print UNITY+" -quit -batchmode -executeMethod ActorEditor.AutoGen  End";
 os.system('cd Assets/Resources/Arts && svn add * --force && svn ci -m "KOL ArtsRes"');
 os.system('cd Assets/Project/Arts && svn add * --force && svn ci -m "KOL ArtsRes"');
